9370a0af512b440d9aaeee56c340ece6595da431
Mohamed <mohammed.korany.ali@gmail.com>
2020-07-14 14:58:16 +0200

    Initial Commit

---
+++ GraphQLDemoCocoaPod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GraphQLDemoCocoaPod
+//
+//  Created by Mohamed Korany Ali on 7/14/20.
+//  Copyright © 2020 Mohamed Korany Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
